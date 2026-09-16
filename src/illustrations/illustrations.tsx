1967a08681bb9e734e3ee1fed04c7bbdd6e1ac13
RinaSannomiya <tzlt.73.san@gmail.com>
2026-09-16 13:05:35 +0800

    Update illustrations.tsx

--- src/illustrations/illustrations.tsx
+++ src/illustrations/illustrations.tsx
@@ -311,6 +311,7 @@ const PLANT_ILLUSTRATIONS: IllustrationDef[] = PLANT_DATA.map((item) => ({
       {...props}
       title={item.title}
       source={`/lineart/plants/plant-${item.page}.png`}
+      allowEdgeFill
     />
   ),
 }))
