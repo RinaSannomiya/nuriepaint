4864119966116c581b218a6a930e60422d7a5418
RinaSannomiya <tzlt.73.san@gmail.com>
2026-09-16 14:10:25 +0800

    Update FlagPage.tsx

--- src/illustrations/svgs/FlagPage.tsx
+++ src/illustrations/svgs/FlagPage.tsx
@@ -11,6 +11,7 @@ export function FlagPage(props: {
   color?: string
   command?: RasterPaintCommand | null
   eyedropper?: boolean
+  brush?: boolean
   onPickColor?: (color: string) => void
   restoreImage?: { url: string; seq: number } | null
 }) {
@@ -22,6 +23,7 @@ export function FlagPage(props: {
       color={props.color}
       command={props.command}
       eyedropper={props.eyedropper}
+      brush={props.brush}
       onPickColor={props.onPickColor}
       restoreImage={props.restoreImage}
     />
